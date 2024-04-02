388898a4c887b17d99d92bb41ea6e779110729a1
Kasthu Mathan <kasthumathanbts@gmail.com>
2024-04-02 17:46:32 +0530

    minor refractor

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div name='about' className='w-full h-screen bg-gradient-to-b from-gray-600 to-black text-white'>
         <div className='max-w-screen-lg p-9 mx-auto flex flex-col justify-center w-full h-full'>
-            <div className='pb-8 text-center'>
+            <div className='pb-8 text-center mt-24'>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-300 text-gray-300'>About</p>
             </div>
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,13 +8,13 @@ const Home = () => {
   return (
     <div name='home' className='h-screen w-full bg-gradient-to-b from-black to-gray-600'>
         <div className='max-w-screen-lg mx-auto flex flex-col items-center h-full px-4 md:flex-row'>
-            <div className='flex flex-col mt-10 justify-center h-full md:mr-4'>
+            <div className='flex flex-col mt-32 md:mt-0 justify-center h-full md:mr-4'>
                 <h2 className='text-4xl sm:text-7xl font-bold text-gray-400 md:text-left'>
                     I am a WordPress Developer & SEO Specialist.
                 </h2>
-                <p className='text-slate-300 py-4 max-w-md md:text-left'>I am an experienced WordPress Developer specializing in crafting visually stunning and highly functional websites optimized for SEO. With expertise in HTML, CSS, and Javascript. I create responsive designs that captivate users and drive traffic. Explore my portfolio to see the fusion of creativity and technical prowess defining my work.</p>
+                <p className='text-slate-300 py-4 max-w-md md:text-left mx-auto md:mx-0'>I am an experienced WordPress Developer specializing in crafting visually stunning and highly functional websites optimized for SEO. With expertise in HTML, CSS, and Javascript. I create responsive designs that captivate users and drive traffic. Explore my portfolio to see the fusion of creativity and technical prowess defining my work.</p>
 
-                <div className='text-center md:text-left'>
+                <div className='text-center md:text-left mx-auto md:mx-0'>
                     <Link to='portfolio' smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-indigo-600 to-indigo-500 cursor-pointer'>
                         Portfolio
                         <span className='group-hover:rotate-90 duration-300'>
